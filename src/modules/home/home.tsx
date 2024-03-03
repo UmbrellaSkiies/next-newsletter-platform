993d2e53bce04a160b2b93163d1a9eb5dd14569d
NeoDev <thomasneo946@gmail.com>
2024-03-03 21:45:25 +0200

    Initial commit

---
+++ src/modules/home/home.tsx
@@ -0,0 +1,13 @@
+import Header from '@/shared/widgets/header/header';
+import Banner from './elements/banner';
+
+const Home = () => {
+  return (
+    <div>
+        <Header />
+        <Banner />
+    </div>
+  );
+};
+
+export default Home;
